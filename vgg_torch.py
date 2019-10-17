a062697d00763281681e298d2114c0cb1a1dcf8a
vefs <weichen.vefs@gmail.com>
2019-10-17 21:44:25 +0800

    Add files via upload

--- vgg_torch.py
+++ vgg_torch.py
@@ -1,4 +1,3 @@
-'''Train CIFAR10 with PyTorch.'''
 from __future__ import print_function
 #import cv2
 from PIL import Image
@@ -15,47 +14,26 @@ import torchvision.models as models
 import torchvision.transforms as transforms
 
 ######################
-import matplotlib.pyplot as plt
-import matplotlib.image as mpimg
 import numpy as np
 
 import os
 import datetime
 ####################################################
 
-patb = 'dataset_small_'
-train_dir = './dataset_small/train/'
+patb = 'dataset'
+#train_dir = './dataset_small/train/'
+train_dir = './dataset/train/'
 
 #eval_dir  = './dataset_small/test/'
-eval_dir  = './dataset_small/test2/'
+eval_dir = './dataset/test/'
 
 result_dir  = './result/'
 ti_now = datetime.datetime.now()
 ti_clock =  ti_now.strftime("%m_%d_%H_%M")
 
-result_csv = result_dir+patb+ti_clock+'.csv'
-####################################################
+result_csv = result_dir+patb+'_'+ti_clock+'.csv'
 
-'''
-class DatasetFromFolder(data.Dataset):
-    def __init__(self, image_dir, input_transform=None):
-        super(DatasetFromFolder, self).__init__()
-        self.image_filenames = [join(image_dir, x) for x in listdir(image_dir) if is_image_file(x)]
-        self.input_transform = input_transform
-
-    def __getitem__(self, index):
-        input = load_img(self.image_filenames[index])
-        target = input.copy()
-        if self.input_transform:
-            input = self.input_transform(input)
-
-        return input, target
-
-    def __len__(self):
-        return len(self.image_filenames)
-
-'''
-
+print(' train=', train_dir,'\n eval=', eval_dir, '\n result= ', result_csv)
 
 #################################################
 def is_image_file(filename):
@@ -67,7 +45,9 @@ def load_img(filepath):
 
 class MyDataset(data.Dataset):
     def __init__(self, root, transform=None):
-        img_p = [os.path.join(root, x) for x in os.listdir(root) if is_image_file(x)]
+        eval_lst = os.listdir(root)
+        eval_lst.sort()
+        img_p = [os.path.join(root, x) for x in eval_lst if is_image_file(x)]
         self.root = root
         self.transform = transform
         self.img_p = img_p
@@ -84,22 +64,22 @@ class MyDataset(data.Dataset):
         return len(self.img_p)
 
 
-#224x224
-print('\n [INFO] Load data.. \n')
+#################################################
 
-'''
-transform_train = transforms.Compose([
-    transforms.Resize(256),
-    transforms.CenterCrop(224),
-    transforms.ToTensor(),
-    transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]),
-])
-'''
-
-transform_train = transforms.Compose([
+transform_train1 = transforms.Compose([
     transforms.Resize(256),
     transforms.CenterCrop(224),
     transforms.RandomHorizontalFlip(),
+    transforms.ToTensor(),
+    transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]),
+])
+
+
+transform_train2 = transforms.Compose([
+    transforms.Resize(256),
+    transforms.RandomCrop(224),
+    transforms.RandomHorizontalFlip(),
+    transforms.RandomVerticalFlip(),
     transforms.ToTensor(),
     transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]),
 ])
@@ -113,12 +93,11 @@ transform_eval = transforms.Compose([
 ])
 
 ####################################################
-# torchvision.datasets.ImageFolder(root, transform=None, target_transform=None
-                               # , loader=<function default_loader>, is_valid_file=None)
-trainset    = torchvision.datasets.ImageFolder(root=train_dir, transform=transform_train)
+print('\n [INFO] Load data..')
 
+trainset    = torchvision.datasets.ImageFolder(root=train_dir, transform=transform_train1)
 #trainloader = torch.utils.data.DataLoader(trainset, batch_size=1, shuffle=True)
-trainloader = torch.utils.data.DataLoader(trainset, batch_size=4, shuffle=False)
+trainloader = torch.utils.data.DataLoader(trainset, batch_size=20, shuffle=False)
 
 testset      = MyDataset(root=eval_dir, transform=transform_eval)
 testloader   = torch.utils.data.DataLoader(testset, batch_size=1, shuffle=False)
@@ -129,103 +108,83 @@ classes = ('bedroom', 'coast', 'forest', 'highway', 'insidecity'
          , 'street', 'suburb', 'tallbuilding')
 
 
-def imshow(img):
-    img = img/2 + 0.5     # unnormalize
-    npimg = img.numpy()
-    plt.imshow(np.transpose(npimg, (1, 2, 0)))
-    plt.show()
-
-
 def check_set():
     print("\n\n\n--------- Check trainset  \n ")
-    #print(trainset.class_to_idx)
-    #print(trainset.imgs[0][0])
 
     for ind, (inputs, labels) in enumerate(trainloader, 0):
-        if(ind < 50):
+        if(ind < 25):
             print ("images size", inputs.size(), "labels: ", labels)
             #print ("inputs: ", inputs, "labels: ", labels)
-            #imshow(torchvision.utils.make_grid(inputs))
-
     print("\n\n\n--------- Check testset  \n ")
 
     for ind, (inputs, target) in enumerate(testloader, 0):
-        if(ind < 20):
+        if(ind < 25):
             print ("images size", inputs.size(), "target: ", target)
-            #print ("inputs: ", inputs, "target: ", target)
-            #imshow(torchvision.utils.make_grid(inputs))
-
-
 
 
 ########################
-net = models.vgg19()
-#net = models.vgg19_bn(num_classes=13)
+#net = models.vgg19()
+net = models.vgg19(pretrained=True)
 #net = models.vgg19_bn(pretrained=True)
 
 if (torch.cuda.is_available()):
     net.cuda()
     torch.backends.cudnn.benchmark = True
 
-
 criterion = nn.CrossEntropyLoss()
-#optimizer = optim.SGD(net.parameters(), lr=0.001)
-optimizer = optim.SGD(net.parameters(), lr=0.0001)
+#optimizer = optim.SGD(net.parameters(), lr=0.001, momentum=0.9)
+optimizer = optim.SGD(net.parameters(), lr=0.0001, momentum=0.9)
 
+
+########################
 def train_img(load_mdl):
-    print('\n [INFO] START Training \n')
+    print('\n [INFO] START Training ')
     if(load_mdl):
         net.load_state_dict(torch.load('./mdl_state/mdl_save'))
 
     net.train()
-    iters = 50
+
+    iters = 2
+    loss_values = []
     for epoch in range(iters):
+        print("----------New Epoch ", epoch)
         running_loss = 0.0
-        print("\n----------New Epoch ", epoch)
         for ind, (inputs, labels) in enumerate(trainloader, 0):
             inputs = inputs.cuda()
             labels = labels.cuda()
-
+            # compute gradient and do SGD step
             optimizer.zero_grad()
 
             outputs = net(inputs)
             loss = criterion(outputs, labels)
             loss.backward()
             optimizer.step()
+            #if(epoch % 5 == 4 and ind % 10 == 9):
+            #    print(" loss.data=", loss.data, "labels=", labels[0])
+            running_loss += loss.item()
+        loss_values.append(running_loss)
+        #print(" running_loss=", running_loss)
+        #print(" loss_values=", loss_values)
 
-            #_, predicted = torch.max(outputs.data, 1)
-            # compute gradient and do SGD step
+    #torch.save(net.state_dict(), './mdl_state/mdl_save')
+    print('\n [INFO] Finished Training ')
+    fo = open('./result/train_1_loss.csv', "w")
+    fo.write("epoch,loss\n")
 
-            '''
-            if(ind == 5):
-                print(" predicted=", predicted[0], "labels=", labels[0])
+    for idx in range(iters):
+        line_str = str(idx)+","+str(loss_values[idx])
+        fo.write(line_str)
+    fo.close()
 
-            if( predicted[0] !=  labels[0]):
-                running_loss += 1
-            if ind % 100 == 99:    # print every 2000 mini-batches
-                running_loss = running_loss/100
-                print(" running_loss=", running_loss, " epoch=", epoch, "ind=", ind)
-                running_loss = 0.0
-            '''
-
-            # print statistics
-            if(ind % 100 == 99):
-                print(" loss.data=", loss.data, "labels=", labels[0])
-
-
-    torch.save(net.state_dict(), './mdl_state/mdl_save')
-    print('\n [INFO] Finished Training \n')
 
 ###############################################
-
-
 def eval_img(load_mdl):
-    print('\n [INFO] Start Evaluation..... \n')
+    print('\n [INFO] Start Evaluation.....')
     if(load_mdl):
         net.load_state_dict(torch.load('./mdl_state/mdl_save'))
     net.eval()
     fo = open(result_csv, "w")
-    fo.write("id, label \n")
+    fo.write("id,label\n")
 
     for ind, (inputs, target) in enumerate(testloader, 0):
         inputs = inputs.cuda()
@@ -236,22 +195,22 @@ def eval_img(load_mdl):
         filen = ''.join(target)
         filen = filen.replace(eval_dir, '')
         filen = filen.replace('.jpg', '')
-        #line_str = str(filen)+", "+str(classes[maxp[0]])+"\n"
-        line_str = str(filen)+", "+str(maxp[0])+"\n"
+        line_str = str(filen)+","+str(classes[maxp[0]])+"\n"
+        #line_str = str(filen)+", "+str(maxp[0])+"\n"
         fo.write(line_str)
-        #if(ind < 10):
-            #print("\n ",ind, "-th outputs ", outputs)
-            #print("\n ",ind, "-th pre= ", predicted)
-            #print("\n ",line_str)
+
     fo.close()
-    print('\n [INFO] Finished Evaluation..... \n')
+    print('\n [INFO] Finished Evaluation.....')
 
 #######################
 #check_set()
 
-load_mdl = 1
+#load_mdl = True
 
-train_img(load_mdl)
+train_img(False)
 
-#eval_img(load_mdl)
+#eval_img(True)
+
+
+
 
